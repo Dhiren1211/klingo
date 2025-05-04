37a4608238ecb52965311b531d8b415241b5454c
Dhirendra Kathayat <107436829+Dhiren1211@users.noreply.github.com>
2025-05-04 22:41:36 +0900

    Delete klingo directory

--- klingo/api/daily_word.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../includes/db.php');
-
-// Pick a random word
-$stmt = $conn->query("SELECT * FROM words ORDER BY RAND() LIMIT 1");
-$word = $stmt->fetch(PDO::FETCH_ASSOC);
-
-header('Content-Type: application/json');
-echo json_encode($word);
--- klingo/includes/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> K-Lingo. All Rights Reserved.</p>
-</footer>
-
-<script src="/assets/js/script.js"></script>
-</body>
-</html>
